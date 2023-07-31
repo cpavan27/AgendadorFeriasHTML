e313b0fa6f8648b4a69fb5f92f6d1877c2bd7276
Caique Pavan <caique.pavan@muchmore.digital>
2023-07-31 14:16:12 -0300

    First commit

---
+++ JS/Model.js
@@ -0,0 +1,14 @@
+export class Dias {
+  constructor(dia, feriado, finalDeSemana){
+    this.dia = dia;
+    this.feriado = feriado;
+    this.finalDeSemana = finalDeSemana;
+  }
+};
+
+export class FeriadosDaCidade {
+  constructor(dia, mes){
+    this.dia = dia;
+    this.mes = mes;
+  }
+};
